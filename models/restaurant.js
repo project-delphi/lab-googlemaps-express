2623c6ec6a40b75a452f2f56ae4460689061d48e
Ravi Kalia <ravkalia@gmail.com>
2018-02-21 11:29:29 -0600

    add geoJSON to mongoose model for restaurants

--- models/restaurant.js
+++ models/restaurant.js
@@ -1,9 +1,12 @@
-const mongoose     = require('mongoose');
-const Schema       = mongoose.Schema;
+const mongoose     = require('mongoose')
+const Schema       = mongoose.Schema
 
 const restaurantSchema = new Schema({
   name: String,
   description: String,
-});
+  location: { type: { type: String }, coordinates: [Number] }
+})
 
-module.exports = mongoose.model('Restaurant', restaurantSchema);
+restaurantSchema.index({ location: '2dsphere' })
+
+module.exports = mongoose.model('Restaurant', restaurantSchema)
